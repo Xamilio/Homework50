1f28e881d1265e9381766686febaad3e63e80b94
Sasha Novak <Xamilio25@gmail.com>
2024-02-08 19:29:14 +0200

    Добавьте файлы проекта.

---
+++ Homework50/Homework50.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace ::std;
+
+template <typename T>
+class Swapper {
+public:
+    static void Swap(T& a, T& b) {
+        T temp = std::move(a);
+        a = std::move(b);
+        b = std::move(temp);
+    }
+};
+int main()
+{
+    int x = 5;
+    int y = 10;
+    std::cout << "До Swap: x = " << x << " y = " << y << endl;
+
+    Swapper<int>::Swap(x, y);
+
+    std::cout << "После Swap: x = " << x << " y = " << y << endl;
+}
